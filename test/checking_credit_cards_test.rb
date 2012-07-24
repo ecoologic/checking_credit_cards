5f6f545a3a4be81ab87f06c3625257ef3b4e5618
ecoologic <erikecoologic@gmail.com>
2012-07-24 18:40:24 +0200

    more integration test (failing)

--- test/checking_credit_cards_test.rb
+++ test/checking_credit_cards_test.rb
@@ -6,8 +6,17 @@ describe CheckingCreditCards do
   describe :present_all do
     it "confirms a valid visa card" do
       # TODO: improve this test with more entries
-      CheckingCreditCards.present_all([4111111111111111]).first.must_match(/^visa(.*)\(valid\)$/)
+      CheckingCreditCards.present_all([4111111111111111]).first.must_match(/^Visa(.*)\(valid\)$/)
     end
+
+    it "confirms an invalid visa card" do
+      CheckingCreditCards.present_all([4111111111111]).first.must_match(/^Visa(.*)\(invalid\)$/)
+    end
+
+    it "confirms an ivalid  mastercard with spaces" do
+      CheckingCreditCards.present_all(['5105 1051 0510 5106']).first.must_match(/^MasterCard(.*)\(invalid\)$/)
+    end
+
   end
 
 end
