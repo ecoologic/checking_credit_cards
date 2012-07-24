e6c1a2aa2f861646cbbcabd3951803b3131002ca
ecoologic <erikecoologic@gmail.com>
2012-07-25 00:39:16 +0200

    correct file names for card_type_detector

--- test/card_type_detector_test.rb
+++ test/card_type_detector_test.rb
@@ -1,23 +1,24 @@
 # http://docs.seattlerb.org/minitest/
 require 'minitest/autorun'
 
+
 describe CardTypeDetector do
 
   describe :card_type_for do
     it "should return visa for a valid visa number" do
-      CardTypeDetector.card_type_for(4111111111111111).must_equal :visa
+      CheckingCreditCards.card_type_for(4111111111111111).must_equal :visa
     end
     
     it "should return mastercard for an invalid mastercard number" do
-      CardTypeDetector.card_type_for(5105105105105106).must_equal :mc
+      CheckingCreditCards.card_type_for(5105105105105106).must_equal :mc
     end
     
     it "should return unknown for a non-credit card number" do
-      CardTypeDetector.card_type_for(51051234123412341).must_equal :unknown
+      CheckingCreditCards.card_type_for(51051234123412341).must_equal :unknown
     end
 
     it "should return unknown for a too long, almost discover card" do
-      CardTypeDetector.card_type_for(601111111111111711).must_equal :unknown
+      CheckingCreditCards.card_type_for(601111111111111711).must_equal :unknown
     end
 
   end
